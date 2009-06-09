30b83ff66792bd69c70f20baae0ead8afb03c4ea
sergey.chernyshev <sergey.chernyshev@7cb82ede-4ecb-11de-8d8d-752d95cf3e3c>
2009-06-09 22:21:51 +0000

    Added bundle info file.
    git-svn-id: http://semantic-mediawiki-bundle.googlecode.com/svn/trunk@23 7cb82ede-4ecb-11de-8d8d-752d95cf3e3c

---
+++ SemanticBundle.php
@@ -0,0 +1,15 @@
+<?
+if ( !defined( 'MEDIAWIKI' ) ) {
+    echo "This file is not a valid entry point.";
+    exit( 1 );
+}
+
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+        'name' => 'SemanticBundle',
+        'description' => 'Semantic Bundle is a pre-packaged bundle of extensions meant to be used on wikis based around the Semantic MediaWiki extension.',
+	'version' => '0.1',
+        'author' => '[http://www.mediawiki.org/wiki/User:Sergey_Chernyshev Sergey Chernyshev] and [http://www.mediawiki.org/wiki/User:Yaron_Koren Yaron Koren]',
+        'url' => 'http://www.mediawiki.org/wiki/Semantic_Bundle'
+);
+
