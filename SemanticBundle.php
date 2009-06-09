cfcea8a63269e02a714ea075047c295239f250ba
sergey.chernyshev <sergey.chernyshev@7cb82ede-4ecb-11de-8d8d-752d95cf3e3c>
2009-06-09 21:51:55 +0000

    Commented out non-default extensions. Using $wgServerName in enableSemantics() call.
    git-svn-id: http://semantic-mediawiki-bundle.googlecode.com/svn/trunk@19 7cb82ede-4ecb-11de-8d8d-752d95cf3e3c

--- SemanticBundle.php
+++ SemanticBundle.php
@@ -7,7 +7,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 # Semantic MediaWiki basic installation.
 # More info: http://semantic-mediawiki.org/wiki/Help:Installation
 include_once("$IP/extensions/SemanticMediaWiki/includes/SMW_Settings.php");
-enableSemantics('');
+enableSemantics($wgServerName);
 
 # Semantic ResultFormats
 # More info: http://semantic-mediawiki.org/wiki/Help:Semantic_Result_Formats#Installation
@@ -19,7 +19,7 @@ include_once("$IP/extensions/SemanticForms/includes/SF_Settings.php");
 
 # Semantic Compound Queries
 # More info: http://www.mediawiki.org/wiki/Extension:Semantic_Compound_Queries
-include_once("$IP/extensions/SemanticCompoundQueries/SCQ_Settings.php");
+#include_once("$IP/extensions/SemanticCompoundQueries/SCQ_Settings.php");
 
 # Semantic Drilldown
 # More info: http://www.mediawiki.org/wiki/Extension:Semantic_Drilldown#Installation
@@ -28,11 +28,11 @@ include_once("$IP/extensions/SemanticDrilldown/includes/SD_Settings.php");
 # Semantic Google Maps 
 # It also requires setting $wgGoogleMapsKey prior to calling it
 # More info: http://www.mediawiki.org/wiki/Extension:Semantic_Google_Maps#Download_and_installation
-include_once( "$IP/extensions/SemanticGoogleMaps/SemanticGoogleMaps.php" );
+#include_once( "$IP/extensions/SemanticGoogleMaps/SemanticGoogleMaps.php" );
 
 # Semantic Tasks
 # More info: http://www.mediawiki.org/wiki/Extension:Semantic_Tasks#Installation
-include_once( "$IP/extensions/SemanticTasks/SemanticTasks.php" );
+#include_once( "$IP/extensions/SemanticTasks/SemanticTasks.php" );
 
 # Admin Links
 # More info: http://www.mediawiki.org/wiki/Extension:Admin_Links#Installation
@@ -40,28 +40,28 @@ include_once("$IP/extensions/AdminLinks/AdminLinks.php");
 
 # ArrayExtension
 # More info: http://www.mediawiki.org/wiki/Extension:ArrayExtension#Installation
-include_once("$IP/extensions/ArrayExtension/ArrayExtension.php");
+#include_once("$IP/extensions/ArrayExtension/ArrayExtension.php");
 
 # Data Transfer
 # More info: http://www.mediawiki.org/wiki/Extension:Data_Transfer#Installation
-include_once("$IP/extensions/DataTransfer/includes/DT_Settings.php");
+#include_once("$IP/extensions/DataTransfer/includes/DT_Settings.php");
 
 # External Data
 # It also requires setting $wgGoogleMapsKey prior to calling it
 # More info: http://www.mediawiki.org/wiki/Extension:External_Data#Installation
-include_once("$IP/extensions/ExternalData/ExternalData.php");
+#include_once("$IP/extensions/ExternalData/ExternalData.php");
 
 # Header Tabs
 # More info: http://www.mediawiki.org/wiki/Extension:Header_Tabs#Installation 
-include_once("$IP/extensions/HeaderTabs/HeaderTabs.php");
+#include_once("$IP/extensions/HeaderTabs/HeaderTabs.php");
 
 # Google Geocoder
 # More info: http://www.mediawiki.org/wiki/Extension:Google_Geocoder#Configuration
-require_once("$IP/extensions/GoogleGeocoder/GoogleGeocoder.php");
+#require_once("$IP/extensions/GoogleGeocoder/GoogleGeocoder.php");
  
 # Page Object Model (POM)
 # More info: http://www.mediawiki.org/wiki/Extension:Page_Object_Model#Installation
-include_once("$IP/extensions/PageObjectModel/PageObjectModel.php");
+#include_once("$IP/extensions/PageObjectModel/PageObjectModel.php");
 
 # ParserFunctions
 # More info: http://www.mediawiki.org/wiki/Extension:ParserFunctions
@@ -69,8 +69,8 @@ require_once( "$IP/extensions/ParserFunctions/ParserFunctions.php" );
 
 # Replace Text
 # More info: http://www.mediawiki.org/wiki/Extension:Replace_Text#Installation
-require_once( "$IP/extensions/ReplaceText/ReplaceText.php" );
-$wgGroupPermissions['sysop']['replacetext'] = true;
+#require_once( "$IP/extensions/ReplaceText/ReplaceText.php" );
+#$wgGroupPermissions['sysop']['replacetext'] = true;
 
 # Widgets
 # Also need to do some permission setup: http://www.mediawiki.org/wiki/Extension:Widgets#Folder_permissions
