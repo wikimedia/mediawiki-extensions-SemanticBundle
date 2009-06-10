130c4c25555304ebaeb39a3e5c6b806e4d6f46f5
yaron57 <yaron57@7cb82ede-4ecb-11de-8d8d-752d95cf3e3c>
2009-06-10 12:34:11 +0000

    Slight improvements
    git-svn-id: http://semantic-mediawiki-bundle.googlecode.com/svn/trunk@27 7cb82ede-4ecb-11de-8d8d-752d95cf3e3c

--- SemanticBundle.php
+++ SemanticBundle.php
@@ -7,9 +7,9 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
         'name' => 'Semantic Bundle',
-        'description' => 'Semantic Bundle is a pre-packaged bundle of extensions meant to be used on wikis based around the Semantic MediaWiki extension.',
+        'description' => 'A pre-packaged bundle of extensions meant to be used on wikis based around the Semantic MediaWiki extension.',
 	'version' => '0.1',
-        'author' => '[http://www.mediawiki.org/wiki/User:Sergey_Chernyshev Sergey Chernyshev] and [http://www.mediawiki.org/wiki/User:Yaron_Koren Yaron Koren]',
+        'author' => array( '[http://www.mediawiki.org/wiki/User:Sergey_Chernyshev Sergey Chernyshev]', '[http://www.mediawiki.org/wiki/User:Yaron_Koren Yaron Koren]' ),
         'url' => 'http://www.mediawiki.org/wiki/Semantic_Bundle'
 );
 
