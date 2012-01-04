49e11a7808568ac8210e6974721ba037a298d62d
jeroendedauw@gmail.com <jeroendedauw@gmail.com@7cb82ede-4ecb-11de-8d8d-752d95cf3e3c>
2012-01-04 01:28:24 +0000

    version gets filled in by makefile
    git-svn-id: http://semantic-mediawiki-bundle.googlecode.com/svn/trunk@173 7cb82ede-4ecb-11de-8d8d-752d95cf3e3c

--- SemanticBundle.php
+++ SemanticBundle.php
@@ -4,11 +4,11 @@ if ( !defined( 'MEDIAWIKI' ) ) {
     exit( 1 );
 }
 
-$wgExtensionCredits[defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'other'][] = array(
+$wgExtensionCredits['semantic'][] = array(
 	'path' => __FILE__,
         'name' => 'Semantic Bundle',
         'description' => 'A pre-packaged bundle of extensions meant to be used on wikis based around the Semantic MediaWiki extension.',
-	'version' => '2011-08-24',
+	'version' => '',
         'author' => array( '[http://www.mediawiki.org/wiki/User:Yaron_Koren Yaron Koren]', '[http://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw]' ),
         'url' => 'http://www.mediawiki.org/wiki/Semantic_Bundle'
 );
