127408cfbcc2c5a92914113dd8ec13f611c31bc8
kghbln <mediawiki@kghoffmeyer.de>
2012-08-29 01:19:32 +0200

    additions and changes
    - i18n added
    - documentation added
    - links updated
    
    Change-Id: Ia63b8c9fa183228cc8fa99b25d496d346e8f60aa

---
+++ SemanticBundle.i18n.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * SemanticBundle - A pre-packaged bundle of extensions meant to be used on wikis
+ * based around the Semantic MediaWiki extension.
+ *
+ * Internationalisation file for the Semantic Bundle
+ *
+ * @file SemanticBundle.i18n.php
+ * @ingroup SemanticBundle
+ */
+
+$messages = array();
+
+/** English
+ * @author Yaron Koren
+ */
+$messages['en'] = array(
+	'semanticbundle-desc'   => 'A pre-packaged bundle of Semantic MediaWiki related extensions',
+);
+
+/** Message documentation (Message documentation)
+  * @author Kghbln
+  */
+$messages['qqq'] = array(
+	'semanticbundle-desc'   => '{{desc}}',
+);
+
--- SemanticBundle.php
+++ SemanticBundle.php
@@ -1,15 +1,42 @@
 <?php
+/**
+ * SemanticBundle - A pre-packaged bundle of extensions meant to be used on wikis
+ * based around the Semantic MediaWiki extension.
+ *
+ * @link https://www.mediawiki.org/wiki/Extension:Semantic_Bundle Documentation
+ *
+ * @file SemanticBundle.php
+ * @defgroup SemanticBundle
+ * @ingroup Extensions
+ * @package MediaWiki
+ * @author Sergey Chernyshev
+ * @author Yaron Koren
+ * @author Jeroen De Dauw
+ * @copyright (C) 2008 Sergey Chernyshev and Yaron Koren
+ * @license http://opensource.org/licenses/bsd-license.php The BSD 2-Clause License
+ */
+
 if ( !defined( 'MEDIAWIKI' ) ) {
     echo "This file is not a valid entry point.";
     exit( 1 );
 }
 
 $wgExtensionCredits['semantic'][] = array(
-	'path' => __FILE__,
-        'name' => 'Semantic Bundle',
-        'description' => 'A pre-packaged bundle of extensions meant to be used on wikis based around the Semantic MediaWiki extension.',
-	'version' => '',
-        'author' => array( '[http://www.mediawiki.org/wiki/User:Yaron_Koren Yaron Koren]', '[http://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw]' ),
-        'url' => 'http://www.mediawiki.org/wiki/Semantic_Bundle'
+	'path'           => __FILE__,
+	'name'           => 'Semantic Bundle',
+	'descriptionmsg' => 'semanticbundle-desc',
+	'version'        => '',
+	'author'         => array(
+		'[https://www.mediawiki.org/wiki/User:Yaron_Koren Yaron Koren]',
+		'[https://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw]',
+		'...'
+		),
+	'url'            => 'https://www.mediawiki.org/wiki/Semantic_Bundle'
 );
+
+// define server-local path to this file
+$dir = dirname( __FILE__ );
+
+// register message file
+$wgExtensionMessagesFiles['SemanticBundle'] = $dir . '/SemanticBundle.i18n.php';
 
